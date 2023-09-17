a1a244ec4ddc5a0e364aa12ba02a8b7c8215d466
saku-1101 <sakupi1101@outlook.jp>
2023-09-17 10:31:13 +0900

    chore: eslint settings

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -8,3 +8,24 @@ const buildEslintCommand = (filenames) =>
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
 }
+
+// exclude files in eslintignore from lint checking
+const { ESLint } = require('eslint')
+
+const removeIgnoredFiles = async (files) => {
+  const eslint = new ESLint()
+  const isIgnored = await Promise.all(
+    files.map((file) => {
+      return eslint.isPathIgnored(file)
+    }),
+  )
+  const filteredFiles = files.filter((_, i) => !isIgnored[i])
+  return filteredFiles.join(' ')
+}
+
+module.exports = {
+  '**/*.{ts,tsx,js,jsx}': async (files) => {
+    const filesToLint = await removeIgnoredFiles(files)
+    return [`eslint --max-warnings=0 ${filesToLint}`]
+  },
+}
