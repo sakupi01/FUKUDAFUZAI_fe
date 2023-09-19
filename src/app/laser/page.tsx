e6bb2c9af4ff365143f0dcb3e88319986f66a485
shunsuke-tamura <wwwxx4869@gmail.com>
2023-09-20 07:46:20 +0900

    fix: よくわからんけど言われた通りビルドが通るおように書き換え

--- src/app/laser/page.tsx
+++ src/app/laser/page.tsx
@@ -25,100 +25,102 @@ export default function Home() {
   const [users, setUsers] = useState<User[]>([])
 
   useEffect(() => {
-    const id = self.crypto.randomUUID()
-    setThisId(id)
-    const peer = new Peer(id)
+    if (typeof window !== 'undefined') {
+      const id = self.crypto.randomUUID()
+      setThisId(id)
+      const peer = new Peer(id)
 
-    peer.on('connection', (conn) => {
-      conn.on('data', (data) => {
-        const recieved: Message = data as Message
+      peer.on('connection', (conn) => {
+        conn.on('data', (data) => {
+          const recieved: Message = data as Message
 
-        // Message.type handling
-        if (recieved.type === sensorInfo) {
-          setUsers((prev) => {
-            let u: User | undefined
-            prev.forEach((user, index) => {
-              if (user.peerId === conn.peer) {
-                u = user
-                prev.splice(index, 1)
-              }
-            })
-
-            const user: User | undefined = u
-              ? {
-                  id: u.id,
-                  peerId: u.peerId,
-                  name: u.name,
-                  pointer: sensorPerInfoToPointer(recieved.data as SensorPerInfo),
-                  iconColor: u.iconColor,
+          // Message.type handling
+          if (recieved.type === sensorInfo) {
+            setUsers((prev) => {
+              let u: User | undefined
+              prev.forEach((user, index) => {
+                if (user.peerId === conn.peer) {
+                  u = user
+                  prev.splice(index, 1)
                 }
-              : u
-            const newUsers = user ? [...prev, user] : prev
-            // sort by id
-            newUsers.sort((a, b) => {
-              if (a.id < b.id) return -1
-              if (a.id > b.id) return 1
-              return 0
+              })
+
+              const user: User | undefined = u
+                ? {
+                    id: u.id,
+                    peerId: u.peerId,
+                    name: u.name,
+                    pointer: sensorPerInfoToPointer(recieved.data as SensorPerInfo),
+                    iconColor: u.iconColor,
+                  }
+                : u
+              const newUsers = user ? [...prev, user] : prev
+              // sort by id
+              newUsers.sort((a, b) => {
+                if (a.id < b.id) return -1
+                if (a.id > b.id) return 1
+                return 0
+              })
+              return newUsers
             })
-            return newUsers
-          })
-          setSensorPerInfo(recieved.data as SensorPerInfo)
-        } else if (recieved.type === userSetting) {
-          setUsers((prev) => {
-            console.log(prev)
+            setSensorPerInfo(recieved.data as SensorPerInfo)
+          } else if (recieved.type === userSetting) {
+            setUsers((prev) => {
+              console.log(prev)
 
-            const user: User = {
-              id: prev.length + 1,
-              peerId: conn.peer,
-              name: (recieved.data as UserSetting).name,
-              pointer: { x: 0, y: 0 },
-              iconColor: ['red', 'blue', 'green', 'yellow', 'grey'][
-                prev.length
-              ] as string,
-            }
-            const res: Message = {
-              type: userSetting,
-              data: {
-                id: user.id,
-                name: user.name,
-              } as UserSettingRes,
-            }
-            send(res, conn)
-            return [...prev, user]
-          })
-        } else if (recieved.type === 'shoot') {
-          console.log('shoot')
+              const user: User = {
+                id: prev.length + 1,
+                peerId: conn.peer,
+                name: (recieved.data as UserSetting).name,
+                pointer: { x: 0, y: 0 },
+                iconColor: ['red', 'blue', 'green', 'yellow', 'grey'][
+                  prev.length
+                ] as string,
+              }
+              const res: Message = {
+                type: userSetting,
+                data: {
+                  id: user.id,
+                  name: user.name,
+                } as UserSettingRes,
+              }
+              send(res, conn)
+              return [...prev, user]
+            })
+          } else if (recieved.type === 'shoot') {
+            console.log('shoot')
 
-          // TODO: shoot
+            // TODO: shoot
 
-          // const shoot: Shoot = recieved.data as Shoot
-          // let hit: Target | null = null
-          // setTargets((prev) => {
-          //   console.log(prev)
-          //   if (!prev) return null
+            // const shoot: Shoot = recieved.data as Shoot
+            // let hit: Target | null = null
+            // setTargets((prev) => {
+            //   console.log(prev)
+            //   if (!prev) return null
 
-          //   for (let i = 0; i < prev.length; i++) {
-          //     hit = targetHit(prev[i], sensorPerInfoToPointer(shoot.sensorPerInfo))
-          //     if (hit) {
-          //       prev!.splice(i, 1)
-          //       break
-          //     }
-          //   }
-          //   console.log(sensorPerInfoToPointer(shoot.sensorPerInfo))
+            //   for (let i = 0; i < prev.length; i++) {
+            //     hit = targetHit(prev[i], sensorPerInfoToPointer(shoot.sensorPerInfo))
+            //     if (hit) {
+            //       prev!.splice(i, 1)
+            //       break
+            //     }
+            //   }
+            //   console.log(sensorPerInfoToPointer(shoot.sensorPerInfo))
 
-          //   const res: Message = {
-          //     type: 'shootRes',
-          //     data: {
-          //       score: hit ? hit.score : null,
-          //     } as ShootRes,
-          //   }
-          // send(res, conn)
-          // return prev
-          // })
-        }
+            //   const res: Message = {
+            //     type: 'shootRes',
+            //     data: {
+            //       score: hit ? hit.score : null,
+            //     } as ShootRes,
+            //   }
+            // send(res, conn)
+            // return prev
+            // })
+          }
+        })
+        conn.on('open', () => {})
       })
-      conn.on('open', () => {})
-    })
+    }
   }, [])
 
   const send = (msg: unknown, conn: DataConnection) => {
@@ -126,7 +128,6 @@ export default function Home() {
     const ab = new TextEncoder().encode(JSON.stringify(msg)).buffer
     conn.send(new Uint8Array(ab))
   }
-
   return (
     <>
       {isWaitingRoom && (
