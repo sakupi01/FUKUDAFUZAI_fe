c002c73955c262a5b6b3512f0e3c72a0de13278e
saku-1101 <sakupi1101@outlook.jp>
2023-09-20 01:12:09 +0900

    fix: cannot find module

--- src/util/randomObjFactory.ts
+++ src/util/randomObjFactory.ts
@@ -1,40 +1,40 @@
-import { BallProps, Vector3ObjectBall } from "@/state/slices/ballsSlice";
+import type { BallProps, Vector3ObjectBall } from '@/types/BallTypes'
 
 function getRandomNumber(min: number, max: number) {
-  return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min
 }
 
 export function randomObjectFactory(): Array<Vector3ObjectBall> {
   const RedBall: BallProps = {
     time: 4,
-    color: "#a492f9",
+    color: '#a492f9',
     point: 1,
-  };
+  }
   const YellowBall: BallProps = {
     time: 3,
-    color: "#77ff7e",
+    color: '#77ff7e',
     point: 2,
-  };
+  }
   const GreenBall: BallProps = {
     time: 2,
-    color: "#fff377",
+    color: '#fff377',
     point: 3,
-  };
+  }
   const BlackBall: BallProps = {
     time: 1,
-    color: "#fe3972",
+    color: '#fe3972',
     point: 4,
-  };
-  const ballArr = [RedBall, YellowBall, GreenBall, BlackBall];
+  }
+  const ballArr = [RedBall, YellowBall, GreenBall, BlackBall]
   // const randomCount = 1; // ランダムな個数（1から5個）
-  const randomCount = Math.floor(Math.random() * 5) + 2; // ランダムな個数（1から5個）
-  const objects = [];
+  const randomCount = Math.floor(Math.random() * 5) + 2 // ランダムな個数（1から5個）
+  const objects = []
 
   for (let i = 0; i < randomCount; i++) {
-    const randomX = getRandomNumber(-3.5, 3.5); // xの範囲は-3.5から3.5
-    const randomY = getRandomNumber(-2.4, 1.6); // yの範囲は-2.4から1.6
-    const randomNumber = Math.floor(getRandomNumber(0, 3.9)); // yの範囲は-2.4から1.6
-    const randomBall = ballArr[randomNumber];
+    const randomX = getRandomNumber(-3.5, 3.5) // xの範囲は-3.5から3.5
+    const randomY = getRandomNumber(-2.4, 1.6) // yの範囲は-2.4から1.6
+    const randomNumber = Math.floor(getRandomNumber(0, 3.9)) // yの範囲は-2.4から1.6
+    const randomBall = ballArr[randomNumber] || BlackBall
     const object: Vector3ObjectBall = {
       x: randomX,
       y: randomY,
@@ -42,8 +42,8 @@ export function randomObjectFactory(): Array<Vector3ObjectBall> {
       time: randomBall.time,
       color: randomBall.color,
       point: randomBall.point,
-    };
-    objects.push(object);
+    }
+    objects.push(object)
   }
-  return objects;
+  return objects
 }
