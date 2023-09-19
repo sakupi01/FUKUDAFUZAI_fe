2e3aa386d651da1ad8fd49f71e0a084a738e0c7b
shunsuke-tamura <wwwxx4869@gmail.com>
2023-09-20 07:34:56 +0900

    fix: fix

--- src/components/templates/WaitingComponent/WaitingComponent.stories.ts
+++ src/components/templates/WaitingComponent/WaitingComponent.stories.ts
@@ -3,25 +3,29 @@ import { within } from '@storybook/testing-library'
 import { WaitingComponent } from './WaitingComponent'
 
 import type { Meta, StoryObj } from '@storybook/react'
-export type WaitingComponentProps = {};
+export type WaitingComponentProps = {}
 
 const meta = {
-  title: "WaitingComponent",
+  title: 'WaitingComponent',
   component: WaitingComponent,
   tags: ['autodocs'],
   parameters: {
     layout: 'fullscreen',
   },
-  argTypes: {
-  },
-} satisfies Meta<typeof WaitingComponent>;
-
+  argTypes: {},
+} satisfies Meta<typeof WaitingComponent>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
+    const canvas = within(canvasElement)
   },
-};
+  args: {
+    users: [],
+    id: null,
+    setIsWaitingRoom: () => {},
+    setUsers: () => {},
+  },
+}
