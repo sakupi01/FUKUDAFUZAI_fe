198e6f314b9ffc85974de933751a4edcfbdeeea8
shunsuke-tamura <wwwxx4869@gmail.com>
2023-09-20 07:27:06 +0900

    fix: ようわからんけど、修正

--- src/components/molecules/WaitCard/WaitCard.stories.ts
+++ src/components/molecules/WaitCard/WaitCard.stories.ts
@@ -18,5 +18,10 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  args: {},
+  args: {
+    users: [],
+    id: null,
+    setIsWaitingRoom: () => {},
+    setUsers: () => {},
+  },
 }
