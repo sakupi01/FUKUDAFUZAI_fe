48bf0d86175362caba2942a11fecfa35dc8c784e
smaru1111 <96244711+smaru1111@users.noreply.github.com>
2023-09-20 08:29:23 +0000

    chore: test marusou

---
+++ src/app/marusou/page.tsx
@@ -0,0 +1,73 @@
+'use client'
+import { Box } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { Physics } from '@react-three/rapier'
+import { Suspense, useState, useEffect } from 'react'
+import { Vector3 } from 'three'
+
+import type { User } from '@/types/User'
+
+export type PlayGroundForLaserProps = {
+  users: User[]
+}
+
+export const PlayGroundForLaser = ({ ...props }: PlayGroundForLaserProps) => {
+  const [cameraParams, setCameraParams] = useState({
+    windowHeight: 0,
+    windowWidth: 0,
+  })
+  useEffect(() => {
+    setCameraParams({
+      windowHeight: innerHeight,
+      windowWidth: innerWidth,
+    })
+  }, [])
+  const distance = 800
+  const FOV = (2 * Math.atan(cameraParams.windowHeight / (2 * distance)) * 180) / Math.PI
+
+  const [cameraPosition, setCameraPosition] = useState(new Vector3(0, 2, 5))
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const randomPosition = new Vector3(
+        Math.random() * 10 - 5, // X座標をランダムに設定（-5から5の範囲）
+        Math.random() * 4 + 1, // Y座標をランダムに設定（1から5の範囲）
+        Math.random() * 10 - 5, // Z座標をランダムに設定（-5から5の範囲）
+      )
+      setCameraPosition(randomPosition)
+    }, 2200) // 2秒ごとにカメラポジションを更新
+
+    return () => clearInterval(intervalId) // コンポーネントがアンマウントされたらインターバルをクリア
+  }, [])
+
+  console.log(cameraPosition)
+
+  return (
+    <Canvas
+      shadows
+      gl={{ localClippingEnabled: true }}
+      camera={{
+        position: cameraPosition,
+        fov: FOV,
+        aspect: cameraParams.windowWidth / cameraParams.windowHeight,
+      }}
+      style={{ width: '100vw', height: '100vh' }}
+    >
+      <ambientLight />
+      <color attach='background' args={['#fff']} />
+      <Suspense>
+        <Physics
+          debug
+          interpolate={true}
+          maxVelocityIterations={1}
+          maxVelocityFrictionIterations={2}
+          gravity={[0, -40, 0]}
+        >
+          <Box />
+        </Physics>
+      </Suspense>
+    </Canvas>
+  )
+}
+
+export default PlayGroundForLaser
