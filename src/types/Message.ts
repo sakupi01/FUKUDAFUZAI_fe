763d641a5f511904082a8d909d3cc6331853b000
chouette11 <keifukuda1115@gmail.com>
2023-09-21 14:03:09 +0900

    feat: add colors

--- src/types/Message.ts
+++ src/types/Message.ts
@@ -22,7 +22,7 @@ export type UserSetting = {
   name: string
 }
 
-export const colors = ['FF0000', '0000FF', '008000', 'FFFF00', '808080']
+export const colors = [  "FF0000",  "00FF00",  "0000FF",  "FFFF00",  "FF00FF",  "00FFFF",  "FFFFFF",  "000000",  "800000",  "008000",  "000080",  "808000",  "800080",  "008080",  "C0C0C0",  "FFA500",  "A52A2A",  "8B4513",  "2E8B57",  "D2691E",  "CD5C5C",  "B0E0E6",  "32CD32",  "FA8072",  "E9967A",  "FFC0CB",  "DDA0DD",  "BDB76B",  "FFD700",  "20B2AA"]
 export const LASER_COLORS = ['#89d3f0', '#f7fdab', '#f693f1', '#abf0bf', '#c29cff']
 export type UserSettingRes = {
   id: number
@@ -32,6 +32,7 @@ export type UserSettingRes = {
 
 export type Shoot = {
   id: number
+  charge: number
   sensorPerInfo: SensorPerInfo
 }
 
