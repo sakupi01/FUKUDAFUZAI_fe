834d901116bd1dc16234f073c66dbb0669c0299e
saku-1101 <sakupi1101@outlook.jp>
2023-09-21 11:02:23 +0900

    chore: args add

--- src/components/molecules/HitMaterials/HitMaterials.stories.tsx
+++ src/components/molecules/HitMaterials/HitMaterials.stories.tsx
@@ -18,6 +18,21 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
+  args: {
+    balls: [
+      {
+        x: 0,
+        y: 0,
+        z: 0,
+        id: 1,
+        time: 4,
+        color: '#a492f9',
+        point: 1,
+        isDeleted: false,
+      },
+    ],
+    setBalls: () => {},
+  },
   decorators: [
     (Story) => (
       <CanvasProvider>
